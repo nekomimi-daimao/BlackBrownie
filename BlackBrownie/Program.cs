7b2ef2e73f70d8db35bd40a177f1007293c8e1ee
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2023-11-25 00:56:27 +0900

    機能を選択して引数を受け取れるようにする

--- BlackBrownie/Program.cs
+++ BlackBrownie/Program.cs
@@ -9,8 +9,28 @@ IFunction[] functions =
 for (var index = 0; index < functions.Length; index++)
 {
     var function = functions[index];
-    Console.WriteLine(index);
-    Console.WriteLine(function.DescriptionFunction());
+    Console.WriteLine($"[{index:00}] {function.DescriptionFunction()}");
     Console.WriteLine("---------");
 }
 
+Console.WriteLine("input index");
+var readLineIndex = Console.ReadLine();
+if (!int.TryParse(readLineIndex, out var i))
+{
+    Console.WriteLine("not index");
+    return;
+}
+
+if (i < 0 || i >= functions.Length)
+{
+    Console.WriteLine("out of range");
+    return;
+}
+
+var f = functions[i];
+Console.WriteLine($"[{f.GetType().Name}] input args");
+Console.WriteLine(f.DescriptionArgs());
+var readLineArgs = Console.ReadLine();
+var argsArray = readLineArgs?.Split(new[] { ' ', '　' }, StringSplitOptions.RemoveEmptyEntries)
+                ?? Array.Empty<string>();
+f.Do(argsArray);
