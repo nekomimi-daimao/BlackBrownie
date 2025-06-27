012c460e0099f199a398a3733d6deeb5e55d17a8
nekomimi-daimao <35223467+nekomimi-daimao@users.noreply.github.com>
2025-06-28 00:09:30 +0900

    boot all cli

--- BlackBrownie/Program.cs
+++ BlackBrownie/Program.cs
@@ -18,30 +18,9 @@ IFunction[] functions =
     new FunctionDownsizeImage(),
 };
 
-for (var index = 0; index < functions.Length; index++)
-{
-    var function = functions[index];
-    Console.WriteLine($"[{index:00}] {function.DescriptionFunction()}");
-    Console.WriteLine("---------");
-}
-
-Console.WriteLine("input index");
-var readLineIndex = Console.ReadLine();
-if (!int.TryParse(readLineIndex, out var i))
-{
-    Console.WriteLine("not index");
-    return;
-}
-
-if (i < 0 || i >= functions.Length)
-{
-    Console.WriteLine("out of range");
-    return;
-}
-
 var cts = new CancellationTokenSource();
 var token = cts.Token;
-Console.CancelKeyPress += (sender, eventArgs) =>
+Console.CancelKeyPress += (_, eventArgs) =>
 {
     eventArgs.Cancel = true;
     if (!cts.IsCancellationRequested)
@@ -51,10 +30,68 @@ Console.CancelKeyPress += (sender, eventArgs) =>
     }
 };
 
-var f = functions[i];
-Console.WriteLine($"[{f.GetType().Name}] input args");
-Console.WriteLine(f.DescriptionArgs());
-var readLineArgs = Console.ReadLine();
-var argsArray = readLineArgs?.Split(new[] { ' ', '　' }, StringSplitOptions.RemoveEmptyEntries)
-                ?? Array.Empty<string>();
-await f.Do(argsArray, token);
+int selectedFunctionIndex;
+string[] selectedFunctionArgs;
+
+if (args.Length > 0 && int.TryParse(args[0], out var bootIndex) && CheckIsRange(bootIndex))
+{
+    selectedFunctionIndex = bootIndex;
+    selectedFunctionArgs = args.Skip(1).ToArray();
+    var function = functions[selectedFunctionIndex];
+    Console.WriteLine($"[{selectedFunctionIndex:00}] {function.DescriptionFunction()}");
+}
+else
+{
+    var (index, strings) = InteractiveMode();
+    selectedFunctionIndex = index;
+    selectedFunctionArgs = strings;
+}
+
+if (!CheckIsRange(selectedFunctionIndex))
+{
+    Console.WriteLine("out of range");
+    return;
+}
+
+token.ThrowIfCancellationRequested();
+
+var selectedFunction = functions[selectedFunctionIndex];
+await selectedFunction.Do(selectedFunctionArgs, token);
+return;
+
+(int index, string[] args) InteractiveMode()
+{
+    for (var index = 0; index < functions.Length; index++)
+    {
+        var function = functions[index];
+        Console.WriteLine($"[{index:00}] {function.DescriptionFunction()}");
+        Console.WriteLine("---------");
+    }
+
+    Console.WriteLine("input index");
+
+    token.ThrowIfCancellationRequested();
+
+    var readLineIndex = Console.ReadLine();
+    if (!int.TryParse(readLineIndex, out var i))
+    {
+        Console.WriteLine("not index");
+        return (-1, []);
+    }
+
+    token.ThrowIfCancellationRequested();
+
+    var f = functions[i];
+    Console.WriteLine($"[{f.GetType().Name}] input args");
+    Console.WriteLine(f.DescriptionArgs());
+    var readLineArgs = Console.ReadLine();
+    var argsArray = readLineArgs?.Split(new[] { ' ', '　' }, StringSplitOptions.RemoveEmptyEntries)
+                    ?? Array.Empty<string>();
+
+    return (i, argsArray);
+}
+
+bool CheckIsRange(int targetIndex)
+{
+    return targetIndex >= 0 && targetIndex < functions.Length;
+}
